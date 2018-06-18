03c557d114aaeb3181332883845f0071008069c8
Arthur Jen <arthurljen@gmail.com>
2018-06-18 13:53:57 -0700

    seed data complete

--- scripts/create-tables.js
+++ scripts/create-tables.js
@@ -11,6 +11,11 @@ client.query(`
       linkedin VARCHAR(128)
     );
 
+    CREATE TABLE IF NOT EXISTS tables (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(64) NOT NULL
+    );
+
     CREATE TABLE IF NOT EXISTS humor (
       id SERIAL PRIMARY KEY,
       image_url VARCHAR(512) NOT NULL
@@ -19,13 +24,30 @@ client.query(`
     CREATE TABLE IF NOT EXISTS advice (
       id SERIAL PRIMARY KEY,
       user_id INTEGER NOT NULL REFERENCES users(id),
+      title VARCHAR(256) NOT NULL,
       text VARCHAR(1024) NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS votes (
+      id SERIAL PRIMARY KEY,
+      user_id INTEGER NOT NULL REFERENCES users(id),
+      table_id INTEGER NOT NULL REFERENCES tables(id),
+      post_id INTEGER NOT NULL 
+    );
+    
+    CREATE TABLE IF NOT EXISTS comments (
+      id SERIAL PRIMARY KEY,
+      user_id INTEGER NOT NULL REFERENCES users(id),
+      table_id INTEGER NOT NULL REFERENCES tables(id),
+      post_id INTEGER NOT NULL,
+      text VARCHAR(512) NOT NULL
     );
 
     CREATE TABLE IF NOT EXISTS saved (
       id SERIAL PRIMARY KEY,
       user_id INTEGER NOT NULL REFERENCES users(id),
-      advice_id INTEGER NOT NULL REFERENCES advice(id)
+      table_id INTEGER NOT NULL REFERENCES tables(id),
+      post_id INTEGER NOT NULL
     );
 
 `)
