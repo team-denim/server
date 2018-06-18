bd079668f518f342a03ca0ccb0b1c56497ba0476
Easton Gorishek <ieaston26@gmail.com>
2018-06-18 11:58:04 -0700

    set up create/drop tables

--- scripts/create-tables.js
+++ scripts/create-tables.js
@@ -1,17 +1,38 @@
-// const client = require('../db-client');
+require('dotenv').config();
+const client = require('../db-client');
 
-// client.query(`
-//     CREATE TABLE IF NOT EXISTS quadrants (
-//       id SERIAL PRIMARY KEY,
-//       name VARCHAR(256) NOT NULL,
-//       direction VARCHAR(8) UNIQUE NOT NULL
-//     );
+client.query(`
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      first_name VARCHAR(32) NOT NULL,
+      last_name VARCHAR(32) NOT NULL,
+      email VARCHAR(64) UNIQUE NOT NULL,
+      password VARCHAR(64) NOT NULL,
+      linkedin VARCHAR(128)
+    );
 
-// `)
-//   .then(
-//     () => console.log('create tables complete'),
-//     err => console.log(err)
-//   )
-//   .then(() => {
-//     client.end();
-//   });
+    CREATE TABLE IF NOT EXISTS humor (
+      id SERIAL PRIMARY KEY,
+      image_url VARCHAR(512) NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS advice (
+      id SERIAL PRIMARY KEY,
+      user_id INTEGER NOT NULL REFERENCES users(id),
+      text VARCHAR(1024) NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS saved (
+      id SERIAL PRIMARY KEY,
+      user_id INTEGER NOT NULL REFERENCES users(id),
+      advice_id INTEGER NOT NULL REFERENCES advice(id)
+    );
+
+`)
+  .then(
+    () => console.log('create tables complete'),
+    err => console.log(err)
+  )
+  .then(() => {
+    client.end();
+  });
