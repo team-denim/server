4372357baba7ed66719abe6d8e182bfee90a8705
Arthur Jen <arthurljen@gmail.com>
2018-06-18 15:36:58 -0700

    get advice complete

--- server.js
+++ server.js
@@ -19,14 +19,21 @@ const client = require('./db-client');
 app.get('/api/advice', (req, res) => {
 
   client.query(`
-  SELECT a.id, a.title, a.text, a.user_id,
-    COUNT(votes.id) AS upvotes
-  FROM advice a
-
-  LEFT JOIN votes
-    ON votes.table_id = 1 AND a.id = votes.post_id
-  GROUP BY a.id
-  ORDER BY upvotes DESC
+    SELECT
+      a.id,
+      a.title,
+      a.text,
+      a.user_id,
+      u.first_name,
+      u.last_name,
+      COUNT(v.id) AS upvotes
+    FROM advice a
+    JOIN users u
+      ON u.id = a.user_id
+    LEFT JOIN votes v
+      ON v.table_id = 1 AND a.id = v.post_id
+    GROUP BY a.id, u.first_name, u.last_name
+    ORDER BY upvotes DESC;
 
   `).then(result => {
     res.send(result.rows);
